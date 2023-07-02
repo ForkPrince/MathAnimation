f5766526c8d8ee6f76beb80e93a6b8bb7699762d
Prince527 <Prince527gaming@gmail.com>
2023-07-02 14:27:30 -0700

    Delete Animations directory

--- Animations/include/utils/FontAwesome.h
+++
@@ -1,7 +0,0 @@
-#ifndef MATH_ANIM_FONT_AWESOME_H
-#define MATH_ANIM_FONT_AWESOME_H
-
-#include "IconsFontAwesome5.h"
-#include "IconsFontAwesome5Brands.h"
-
-#endif
--- Animations/src/SingleIncludeImpls.cpp
+++
@@ -1,9 +0,0 @@
-#define GABE_CPP_UTILS_IMPL
-#include <cppUtils/cppUtils.hpp>
-
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb/stb_image.h>
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include <stb/stb_write.h>
-#define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include <stb/stb_image_resize.h>
